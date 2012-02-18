9e30d41659546c01c9fdd120a5379e0dbd6c8c5c
Chris Moos <chris@tech9computers.com>
2012-02-18 11:42:30 -0700

    update versions to 1.0.0

--- lib/python/setup.py
+++ lib/python/setup.py
@@ -9,7 +9,7 @@ import os
 
 from setuptools import setup
 setup(name='chashring',
-      version = '0.1',
+      version = '1.0.0',
       author="Mike Krieger",
       author_email="mike@instagram.com",
       url="http://github.com/Instagram/",
